cd9d4b52a97745de5f16bd949dae691581c60e13
Shoreaw <shoreacali@gmail.com>
2024-09-08 00:54:39 +0700

    Uji Coba Hitung Pythagoras

---
+++ pythagoras_Shoreaw.py
@@ -0,0 +1,8 @@
+# Menghitung Teorema Pythagoras
+def main():
+    a = float(input("Masukkan nilai a: "))
+    b = float(input("Masukkan nilai b: "))
+    c = (a**2 + b**2)**0.5
+    print("Nilai c adalah: ", c)
+    
+main()
